ec3e54513a3f6501ae304deddf366ea6b0a35ef1
Christian Søndergaard Weatherley <alandryl@live.dk>
2020-03-06 17:00:38 +0100

    update

---
+++ Gravity Puzzle Game/Assets/Scripts/Collectible.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectible : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            SoundPlayer soundPlayer = GameObject.Find("SoundPlayer").GetComponent<SoundPlayer>();
+            soundPlayer.audioToPlay = soundPlayer.audioClipWallTransition;
+            soundPlayer.PlayAudio();
+
+            Destroy(gameObject);
+        }
+    }
+}
