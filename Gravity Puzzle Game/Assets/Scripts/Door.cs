a315e13b58fc338e3861f3ca4f705d358bd7dd34
Christian Søndergaard Weatherley <alandryl@live.dk>
2020-03-12 22:23:29 +0100

    Prop and scene updates.

--- Gravity Puzzle Game/Assets/Scripts/Door.cs
+++ Gravity Puzzle Game/Assets/Scripts/Door.cs
@@ -6,25 +6,53 @@ public class Door : MonoBehaviour
 {
     Animator anim;
     AudioSource audioSource;
+    Activation activator;
+
+    public bool automaticOpen;
 
     public AudioClip audioclipOpen;
+    bool open;
+
 
     void Start()
     {
         anim = GetComponent<Animator>();
         audioSource = GetComponent<AudioSource>();
+        activator = GetComponent<Activation>();
     }
 
 
     void Update()
     {
-        
+        if (activator.activated)
+        {
+            Open();
+        }
+    }
+
+
+    public void Open()
+    {
+        activator.activated = false;
+        open = !open;
+
+        if(open)
+        {
+            anim.SetBool("isOpen", true);
+        }
+        else
+        {
+            anim.SetBool("isOpen", false);
+        }
+
+        audioSource.PlayOneShot(audioclipOpen);
     }
 
     void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Player")
         {
+            open = true;
             anim.SetBool("isOpen", true);
             audioSource.PlayOneShot(audioclipOpen);
         }
@@ -34,6 +62,7 @@ public class Door : MonoBehaviour
     {
         if (other.tag == "Player")
         {
+            open = false;
             anim.SetBool("isOpen", false);
             audioSource.PlayOneShot(audioclipOpen);
         }
